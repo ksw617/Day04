adaad4e72122b14db931cacf406b2d77759e4b7d
lecture <ksw617@gmail.com>
2025-01-16 20:52:08 +0900

    Update cMain.cpp

--- Day04/cMain.cpp
+++ Day04/cMain.cpp
@@ -1,27 +1,58 @@
 #include <stdio.h>
 #include <Windows.h>
 
+enum Color
+{
+	Black,
+	DarkBlue,
+	DarkGreen,
+	DarkCyan,
+	DarkRed,
+	DarkMagenta,
+	DarkYellow,
+	Gray,
+	DarkGray,
+	Blue,
+	Green,
+	Cyan,
+	Red,
+	Magenta,
+	Yellow,
+	White,
+};
+
+void ChangeColor(Color color);
 void SetPosition(int x, int y);
+void HideCursor();
 
 struct Obj
 {
 	int x;
 	int y;
+	Color color;
 	const char* shape;
 
 };
 
 Obj player;
+Obj enemy;
 
 void KeyInput();
 
 
 int main()
 {
+	HideCursor();
 
 	player.x = 10;
 	player.y = 10;
-	player.shape = "옷";
+	player.color = Yellow;
+	player.shape = "■";
+
+	enemy.x = 20;
+	enemy.y = 20;
+	enemy.color = Magenta;
+	enemy.shape = "■";
 
 	while (true)
 	{
@@ -29,7 +60,12 @@ int main()
 
 		KeyInput();
 
+		SetPosition(enemy.x, enemy.y);
+		ChangeColor(enemy.color);
+		printf(enemy.shape);
+
 		SetPosition(player.x, player.y);
+		ChangeColor(player.color);
 		printf(player.shape);
 		Sleep(50);
 	}
@@ -66,4 +102,21 @@ void SetPosition(int x, int y) // 함수이름은 편한대로
 	pos.X = x * 2;
 	pos.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+
+//커서 보이는거 방지
+void HideCursor()
+{
+	CONSOLE_CURSOR_INFO info;
+	info.bVisible = false;
+	info.dwSize = 1;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
+
+//색깔 바꾸기
+void ChangeColor(Color color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
