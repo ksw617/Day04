0fd21a74fe1de8c856613f28518b35d472443768
lecture <ksw617@gmail.com>
2025-01-16 20:04:31 +0900

    Update cMain.cpp

--- Day04/cMain.cpp
+++ Day04/cMain.cpp
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <Windows.h>
 
+void SetPosition(int x, int y);
+
+struct Obj
+{
+	int x;
+	int y;
+	const char* shape;
+
+};
+
 int main()
 {
+	Obj player;
+	player.x = 10;
+	player.y = 10;
+	player.shape = "옷";
+
 	while (true)
 	{
 		system("cls");
-		printf("Hello world");
+		SetPosition(player.x, player.y);
+		printf(player.shape);
 		Sleep(50);
 	}
 
 	return 0;
+}
+
+void SetPosition(int x, int y) // 함수이름은 편한대로
+{
+	COORD pos;
+	pos.X = x * 2;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
