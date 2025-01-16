c96826f1d5071636bfc7e04c0339bb49fd61c332
lecture <ksw617@gmail.com>
2025-01-16 20:44:30 +0900

    Update cMain.cpp

--- Day04/cMain.cpp
+++ Day04/cMain.cpp
@@ -11,9 +11,14 @@ struct Obj
 
 };
 
+Obj player;
+
+void KeyInput();
+
+
 int main()
 {
-	Obj player;
+
 	player.x = 10;
 	player.y = 10;
 	player.shape = "옷";
@@ -21,12 +26,38 @@ int main()
 	while (true)
 	{
 		system("cls");
+
+		KeyInput();
+
 		SetPosition(player.x, player.y);
 		printf(player.shape);
 		Sleep(50);
 	}
 
 	return 0;
+}
+
+void KeyInput()
+{
+	if (GetAsyncKeyState(VK_LEFT))
+	{
+		player.x--;
+	}
+
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		player.x++;
+	}
+
+	if (GetAsyncKeyState(VK_UP))
+	{
+		player.y--;
+	}
+
+	if (GetAsyncKeyState(VK_DOWN))
+	{
+		player.y++;
+	}
 }
 
 void SetPosition(int x, int y) // 함수이름은 편한대로
